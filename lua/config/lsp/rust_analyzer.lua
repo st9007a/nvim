e6382b91846d4e8994c2f3e1ca1e1cbbf508b9f8
UnoWu <st9007a@gmail.com>
2023-04-19 21:55:51 +0800

    Add root pattern of rust analyzer

--- lua/config/lsp/rust_analyzer.lua
+++ lua/config/lsp/rust_analyzer.lua
@@ -1,1 +1,19 @@
-return {}
+local present, util = pcall(require, "lspconfig.util")
+
+if not present then
+  return
+end
+
+if not table.unpack then
+    table.unpack = unpack
+end
+
+local root_files = {
+  ".git",
+  ".editorconfig",
+  "Cargo.toml",
+}
+
+return {
+  root_dir = util.root_pattern(table.unpack(root_files)),
+}
